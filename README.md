7256881ea8a2d94164ac993d053e0e937fcccf67
Adam Magaluk <AdamMagaluK@gmail.com>
2016-07-29 08:20:33 -0400

    Updated usage to include publish frequency and optionally ignore headers and start api

--- README.md
+++ README.md
@@ -14,8 +14,15 @@ var MemDeviceRegistry = require('./memory_device_registry');
 var MemPeerRegistry = require('./memory_peer_registry');
 var UsageApp = require('zetta-usage-addon');
 
-var app = new UsageApp()
+var options = {};
 
+// Once hub is connected only send metrics every 30 seconds
+//options.publishFrequency = 30000;
+//options.publishHeaders  = true|falsey
+//options.ignoredHeaders = []; Headers to stripout 
+//options.enableApi = true|false. Usage api /usage
+
+var app = new UsageApp(options)
 
 zetta({registry: new MemDeviceRegistry(), peerRegistry: new MemPeerRegistry()})
   .name('server.1')
--- test/test_collection.js
+++ test/test_collection.js
@@ -1,4 +1,5 @@
 var assert = require('assert');
+var request = require('supertest');
 var zetta = require('zetta');
 var zettacluster = require('zetta-cluster');
 
@@ -8,7 +9,7 @@ var UsageApp = require('../');
 describe('ZettaUsage', function() {
   
   it('receives usage from hub', function(done) {
-    var collector = new UsageApp();
+    var collector = new UsageApp({ publishFrequency: 10 });
 
     var cluster = zettacluster({ zetta: zetta })
         .server('cloud', [collector.collect()])
@@ -34,7 +35,7 @@ describe('ZettaUsage', function() {
 
 
   it('receives a usage with active=false when hub disconnects', function(done) {
-    var collector = new UsageApp();
+    var collector = new UsageApp({ publishFrequency: 10000 });
 
     var cluster = zettacluster({ zetta: zetta })
         .server('cloud', [collector.collect()])
@@ -61,7 +62,7 @@ describe('ZettaUsage', function() {
 
   it('will only emit data events at internal set in options', function(done) {
     var interval = 1000;
-    var delay = 3000;
+    var delay = interval*3 + interval/4;
     var collector = new UsageApp({ publishFrequency: interval });
     var dataCollector = function(runtime) {
       runtime.pubsub.subscribe('_peer/connect', function(ev, msg) {
@@ -92,5 +93,93 @@ describe('ZettaUsage', function() {
       }
     });
   })
+
+  it('includes headers that are not ignored', function(done) {
+    var collector = new UsageApp({ publishFrequency: 10, ignoredHeaders: ['host'] });
+
+    var cluster = zettacluster({ zetta: zetta })
+        .server('cloud', [collector.collect()])
+        .server('test1', [Photocell], ['cloud']);
+
+    collector.once('data', function(msg) {
+      assert.equal(Object.keys(msg.headers).length, 4);
+      cluster.stop();
+      setTimeout(done, 10);
+    });
+
+    cluster.run(function(err) {
+      if (err) {
+        done(err);
+      }
+    });
+  });
+
+  it('publishHeaders disables headers in message', function(done) {
+    var collector = new UsageApp({ publishFrequency: 10, publishHeaders: false });
+
+    var cluster = zettacluster({ zetta: zetta })
+        .server('cloud', [collector.collect()])
+        .server('test1', [Photocell], ['cloud']);
+
+    collector.once('data', function(msg) {
+      assert.equal(msg.headers, undefined);
+      cluster.stop();
+      setTimeout(done, 10);
+    });
+
+    cluster.run(function(err) {
+      if (err) {
+        done(err);
+      }
+    });
+  });
+
+  it('api enabled by default /', function(done) {
+    var collector = new UsageApp({ publishFrequency: 10 });
+
+    var cluster = zettacluster({ zetta: zetta })
+        .server('cloud', [collector.collect()])
+        .server('test1', [Photocell], ['cloud']);
+
+    cluster.once('ready', function() {
+      request(cluster.servers['cloud'].httpServer.server)
+        .get('/usage')
+        .expect(function(res) {
+          var json = JSON.parse(res.text);
+          assert(json.class.indexOf('usage') >= 0);
+          assert.equal(json.links.length, 2);
+          assert.equal(json.entities.length, 1);
+        })
+        .end(done);
+    });
+
+    cluster.run(function(err) {
+      if (err) {
+        done(err);
+      }
+    });
+  });
+
+  it('enableApi would disables api when false', function(done) {
+    var collector = new UsageApp({ enableApi: false });
+
+    var cluster = zettacluster({ zetta: zetta })
+        .server('cloud', [collector.collect()])
+        .server('test1', [Photocell], ['cloud']);
+
+    cluster.once('ready', function() {
+      request(cluster.servers['cloud'].httpServer.server)
+        .get('/usage')
+        .expect(404)
+        .end(done);
+    });
+
+    cluster.run(function(err) {
+      if (err) {
+        done(err);
+      }
+    });
+  });
+
    
 });
