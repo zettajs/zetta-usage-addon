51c98c7fa1bb8c2c25ac2be873104262d7e2c91d
Adam Magaluk <AdamMagaluK@gmail.com>
2016-07-29 07:15:20 -0400

    Added tests

---
+++ test/test_collection.js
@@ -0,0 +1,96 @@
+var assert = require('assert');
+var zetta = require('zetta');
+var zettacluster = require('zetta-cluster');
+
+var Photocell = require('zetta-photocell-mock-driver');
+var UsageApp = require('../');
+
+describe('ZettaUsage', function() {
+  
+  it('receives usage from hub', function(done) {
+    var collector = new UsageApp();
+
+    var cluster = zettacluster({ zetta: zetta })
+        .server('cloud', [collector.collect()])
+        .server('test1', [Photocell], ['cloud']);
+
+    collector.once('data', function(msg) {
+      assert.equal(msg.name, 'test1');
+      assert.equal(msg.active, true);
+      assert(msg.connectionId);
+      assert(msg.connected);
+      assert.equal(typeof msg.bytesRead, 'number');
+      assert.equal(typeof msg.bytesWritten, 'number');
+      cluster.stop();
+      setTimeout(done, 10);
+    });
+
+    cluster.run(function(err) {
+      if (err) {
+        done(err);
+      }
+    });
+  });
+
+
+  it('receives a usage with active=false when hub disconnects', function(done) {
+    var collector = new UsageApp();
+
+    var cluster = zettacluster({ zetta: zetta })
+        .server('cloud', [collector.collect()])
+        .server('test1', [Photocell], ['cloud']);
+
+    cluster.on('ready', function() {
+      collector.once('data', function(msg) {
+        assert(msg.disconnected);
+        assert.equal(msg.active, false);
+        cluster.stop();
+        setTimeout(done, 10);
+      });
+
+      cluster.servers['cloud'].httpServer.peers['test1'].close()
+    });
+
+    cluster.run(function(err) {
+      if (err) {
+        done(err);
+      }
+    });
+  });
+
+
+  it('will only emit data events at internal set in options', function(done) {
+    var interval = 1000;
+    var delay = 3000;
+    var collector = new UsageApp({ publishFrequency: interval });
+    var dataCollector = function(runtime) {
+      runtime.pubsub.subscribe('_peer/connect', function(ev, msg) {
+        msg.peer.subscribe('**');
+      });
+    };
+    var cluster = zettacluster({ zetta: zetta })
+        .server('cloud', [collector.collect(), dataCollector])
+        .server('test1', [Photocell], ['cloud']);
+
+    var received = 0;
+    cluster.on('ready', function() {
+      collector.on('data', function(msg) {
+        received++;
+      });
+    });
+
+    setTimeout(function() {
+      var c = delay/interval;
+      assert(received > c-1 && received < c+1, 'should have received ' + c + ' but instead received ' + received);
+      cluster.stop();
+      setTimeout(done, 10);
+    }, delay)
+
+    cluster.run(function(err) {
+      if (err) {
+        done(err);
+      }
+    });
+  })
+   
+});
